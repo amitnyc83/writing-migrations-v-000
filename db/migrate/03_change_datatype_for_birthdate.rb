c0554ae12e4b22816fde36e641ab87e9130d9f1c
Amit Singh <amit_nyc@live.com>
2018-09-01 20:30:54 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class ChangeColumTypeFromStringToDateTimeForStudents < ActiveRecord::Migration[4.2]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[4.2]
 
 def change
  change_column :students, :birthdate, :integer
